ef8657e4725170f019e1824c402e9f820bfb343a
kx9 <kuangxq2022@163.com>
2024-05-30 19:59:44 +0800

    first commit

---
+++ token/token.go
@@ -0,0 +1,46 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 标识符+字面量
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// 运算符
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// 分隔符
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// 关键字
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
